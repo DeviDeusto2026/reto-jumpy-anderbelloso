46cc539026e2d738b628ae456b6c8836593ef6cd
Ander Belloso <ander.belloso@opendeusto.es>
2026-04-26 13:12:52 +0200

    Jump and right/left movement added

---
+++ Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is 
+    public int movSpeed = 15;
+    public float fuerzaSalto = 5f;
+    private bool canJump;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        move();
+        jump();
+    }
+
+    private void move()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += Vector3.left * Time.deltaTime * movSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += Vector3.right * Time.deltaTime * movSpeed;
+        }
+    }
+
+    private void jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            canJump = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            canJump = true;
+        }
+    }
+}
